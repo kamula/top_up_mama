f4c2a1da5acc8d465c75fb5a4827af377931c8a3
Kamula <kamulaisaac@gmail.com>
2022-03-21 16:05:40 +0300

    adding book comment database orm structure

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
+from statistics import mode
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class BookComment(models.Model):
+    book_id = models.UUIDField(primary_key = True,default = uuid.uuid4,editable = False)
+    id = models.IntegerField()
+    comment = models.CharField(max_length=300)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
